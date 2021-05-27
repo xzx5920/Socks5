62ee2cec84d8a77e0bc21eededefbfb1a509a86c
xzx5920 <84929039+xzx5920@users.noreply.github.com>
2021-05-28 01:15:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -120,7 +120,7 @@ chmod +x /usr/local/bin/s5
 #设置默认用户名、默认开启帐号验证
 uname="123456"
 upasswd="654321"
-port="5555"
+port="1111"
 confFile=/etc/opt/ss5/ss5.conf
 echo -e $uname $upasswd >> /etc/opt/ss5/ss5.passwd
 sed -i '87c auth    0.0.0.0/0               -               u' $confFile
